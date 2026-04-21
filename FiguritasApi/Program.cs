9118c7ef8dd20340c0a51124ecfbd959bf15f37a
tralph3 <tomasralph2000@gmail.com>
2026-04-21 18:48:17 -0300

    redirect to swagger on debug only

--- FiguritasApi/Program.cs
+++ FiguritasApi/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Para persistir en memoria las entidades (No sé si esto queda a futuro. De ser así, habría que hacer una clase initializer para no ensuciar este archivo)
@@ -16,30 +16,27 @@ builder.Services.AddSingleton<PropuestaIntercambioRepository>();
 
 // Para que al hacer un POST se puedan pasar los ENUM por su valor en texto en vez de su valor numérico.
 builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
+    .AddJsonOptions(options => {
         options.JsonSerializerOptions.Converters.Add(
             new JsonStringEnumConverter());
     });
 
 var app = builder.Build();
 
-// Para que abra directo la vista en Swagger
-app.Use(async (context , next) =>
-{
-    if (context.Request.Path == "/")
-    {
-        context.Response.Redirect("/swagger/index.html", permanent:false);
-        return;
-    }
-    await next();
-});
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    // Para que abra directo la vista en Swagger
+    app.Use(async (context , next) => {
+    if (context.Request.Path == "/") {
+        context.Response.Redirect("/swagger/index.html", permanent: false);
+        return;
+    }
+    await next();
+    });
 }
 
 app.UseHttpsRedirection();
